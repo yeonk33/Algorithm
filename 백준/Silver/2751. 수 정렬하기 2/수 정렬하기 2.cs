6fb232f1d29e2c5a1537614acbd00509baf88469
이연경 <55737485+yeonk33@users.noreply.github.com>
2024-12-07 17:52:20 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1260 ms, Memory: 69432 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cs
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cs
@@ -1,11 +1,11 @@
 int inputNum = Convert.ToInt32(Console.ReadLine());
 
-int[] numbers = new int[inputNum];
+List<int> numbers = new List<int>();
 
 for (int i = 0; i < inputNum; i++) {
-	numbers[i] = Convert.ToInt32(Console.ReadLine());
+	numbers.Add(Convert.ToInt32(Console.ReadLine()));
 }
 
-Array.Sort(numbers);
+numbers.Sort();
 
 Console.WriteLine(string.Join("\n", numbers));
