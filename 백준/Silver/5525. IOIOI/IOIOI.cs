38b121cdd01da9c2672050f19538599b0e3a067b
이연경 <55737485+yeonk33@users.noreply.github.com>
2025-04-02 23:30:26 +0900

    [Silver I] Title: IOIOI, Time: 1608 ms, Memory: 17920 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cs
+++ 백준/Silver/5525. IOIOI/IOIOI.cs
@@ -1,16 +1,54 @@
+// 실패함수 생성
+using System.Text;
+
+int[] CreateFail(string pattern)
+{
+	int[] fail = new int[pattern.Length];
+	int j = 0;
+
+	for (int i = 1; i < pattern.Length; i++) {
+		while (j > 0 && pattern[i] != pattern[j]) {
+			j = fail[j - 1];
+		}
+
+		if (pattern[i] == pattern[j]) {
+			j++;
+			fail[i] = j;
+		}
+	}
+
+	return fail;
+}
+
 StreamReader sr = new StreamReader(Console.OpenStandardInput());
+StringBuilder pattern = new StringBuilder();
 
 int N = Convert.ToInt32(sr.ReadLine());
-int length = Convert.ToInt32(sr.ReadLine());
-string inputStr = sr.ReadLine();
-string str = "IOI";
-int count = 0;
+int L = Convert.ToInt32(sr.ReadLine());
+string str = sr.ReadLine();
+pattern.Append("IOI");
+
+// 패턴 생성
 for (int i = 1; i < N; i++) {
-	str += "OI";
+	pattern.Append("OI");
 }
-for (int i = 0; i <= length-str.Length; i++) {
-	if (inputStr.Substring(i, str.Length) == str) {
+
+// 검사
+int[] fail = CreateFail(pattern.ToString());
+int count = 0;
+int j = 0;
+
+for (int i = 0; i < str.Length; i++) {
+	while (j > 0 && str[i] != pattern[j]) {
+		j = fail[j - 1];
+	}
+	
+	if (str[i] == pattern[j])
+		j++;
+
+	if (j == pattern.Length) {
 		count++;
+		j = fail[j - 1];
 	}
 }
 
