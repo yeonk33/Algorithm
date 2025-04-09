f2ef8cb1e9cc9b20311d3f17012e08509e167f30
이연경 <55737485+yeonk33@users.noreply.github.com>
2025-04-09 16:17:13 +0900

    [Silver IV] Title: 듣보잡, Time: 120 ms, Memory: 15272 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cs
+++ 백준/Silver/1764. 듣보잡/듣보잡.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.Text;
 StreamReader sr = new StreamReader(Console.OpenStandardInput());
 StringBuilder sb = new StringBuilder();
@@ -6,16 +7,16 @@ StringBuilder sb = new StringBuilder();
 int[] NM = Array.ConvertAll((sr.ReadLine().Split(' ')), int.Parse);
 int N = NM[0];
 int M = NM[1];
-Hashtable hashtable = new Hashtable();
+HashSet<string> hashtable = new HashSet<string>();
 string name;
 List<string> answers = new List<string>();
 for (int i = 0; i < N; i++) {   // 듣도 못한 사람 해시테이블에 저장
 	name = sr.ReadLine();
-	hashtable.Add(name, name);
+	hashtable.Add(name);
 }
 for (int i = 0; i < M; i++) {   // 보도 못한 사람 입력 받으며 중복 찾기
 	name = sr.ReadLine();
-	if (hashtable.ContainsKey(name)) {
+	if (hashtable.Contains(name)) {
 		answers.Add(name);
 	}
 }
