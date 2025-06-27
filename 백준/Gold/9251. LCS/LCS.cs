65bd771772b3a58df494af3d78d338c7980a16a3
이연경 <55737485+yeonk33@users.noreply.github.com>
2025-06-27 19:38:21 +0900

    [Gold V] Title: LCS, Time: 56 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cs
@@ -0,0 +1,22 @@
+        string str1 = Console.ReadLine();
+        string str2 = Console.ReadLine();
+        int n = str1.Length;
+        int m = str2.Length;
+        int[,] dp = new int[n + 1, m + 1];
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (str1[i - 1] == str2[j - 1])
+                {
+                    dp[i, j] = dp[i - 1, j - 1] + 1;
+                }
+                else
+                {
+                    dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                }
+            }
+        }
+
+        Console.WriteLine(dp[n, m]);
